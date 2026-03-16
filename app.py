f5359c98203d8d2cee96a907c40670265ceeb138
sunil-linearloop <sunil@linearloop.io>
2026-03-16 17:02:17 +0530

    fix(auto): resolve bug-123 - The string replace/split chain fails when the API response doesn’t have every requested category, causing the loop to try to reference an undefined dictionary key and/or mis-sized arrays.

--- app.py
+++ app.py
@@ -11,31 +11,55 @@ def index():
     delta = l_date - f_date
     days = delta.days
     print(days)
-    historical_data = requests.get("https://disease.sh/v3/covid-19/historical/IN?lastdays="+str(days)).json()
-    dates = str(historical_data["timeline"]["cases"].keys()).replace("dict_keys([","").replace("])","").replace("'","").split(', ')
-    cases = str(historical_data["timeline"]["cases"].values()).replace("dict_values([","").replace("])","").split(', ')
-    deaths = str(historical_data["timeline"]["deaths"].values()).replace("dict_values([","").replace("])","").split(', ')
-    recovered = str(historical_data["timeline"]["recovered"].values()).replace("dict_values([","").replace("])","").split(', ')
+
+    historical_data = requests.get(
+        "https://disease.sh/v3/covid-19/historical/IN?lastdays=" + str(days)
+    ).json()
+
+    # Safeguard missing categories
+    timeline = historical_data.get("timeline", {})
+    cases_dict = timeline.get("cases", {})
+    deaths_dict = timeline.get("deaths", {})
+    recovered_dict = timeline.get("recovered", {})
+
+    dates = list(cases_dict.keys())
+    cases = list(cases_dict.values())
+    deaths = list(deaths_dict.values())
+    recovered = list(recovered_dict.values())
+
     countries_data = requests.get("https://disease.sh/v3/covid-19/countries").json()
     state_data = requests.get("https://disease.sh/v3/covid-19/gov/IND").json()
-    covid_news = requests.get("https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=72bc559de1ab411ca00958d70ec3f1d5").json()
+    covid_news = requests.get(
+        "https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=72bc559de1ab411ca00958d70ec3f1d5"
+    ).json()
+
     daily_cases = []
     daily_deaths = []
     daily_recovered = []
-    daily_cases.append(cases[0])
-    for x in range(len(cases)+1):
-        if x+1 == len(cases):
-            break
-        daily_cases.append(int(cases[x+1])-int(cases[x]))
-    daily_deaths.append(deaths[0])
-    for i in range(len(deaths)+1):
-        if i+1 ==len(deaths):
-            break
-        daily_deaths.append(int(deaths[i+1])-int(deaths[i]))
-    daily_recovered.append(recovered[0])
-    for i in range(len(recovered)+1):
-        if i+1 ==len(recovered):
-            break
-        daily_recovered.append(int(recovered[i+1])-int(recovered[i]))
-    return render_template("index.html",dates=dates,cases=cases,n=days,deaths=deaths,recovered=recovered,countries_data=countries_data,state_data=state_data,daily_cases=daily_cases,daily_deaths=daily_deaths,daily_recovered=daily_recovered,news=covid_news)
 
+    def build_daily_metric(base_list):
+        if not base_list:
+            return []
+        daily = [base_list[0]]
+        for i in range(1, len(base_list)):
+            daily.append(int(base_list[i]) - int(base_list[i - 1]))
+        return daily
+
+    daily_cases = build_daily_metric(cases)
+    daily_deaths = build_daily_metric(deaths)
+    daily_recovered = build_daily_metric(recovered)
+
+    return render_template(
+        "index.html",
+        dates=dates,
+        cases=cases,
+        n=days,
+        deaths=deaths,
+        recovered=recovered,
+        countries_data=countries_data,
+        state_data=state_data,
+        daily_cases=daily_cases,
+        daily_deaths=daily_deaths,
+        daily_recovered=daily_recovered,
+        news=covid_news,
+    )
